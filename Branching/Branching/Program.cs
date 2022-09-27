1cec5dcfc512d660669db2890e5d87d6e685cbb5
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-09-27 16:08:39 -0700

    Updated Branching Submission Assignment
    Branching Submission Assignment

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -18,9 +18,8 @@ namespace Branching
 
             // Prints question - asking user for the weight of the package,
             // If package weight is above 50.00, will notify user that its
-            // unable to be shipped via Package Express and program will end.
-            // If below 50.00, will print next question - asking user
-            // for the height of the package.
+            // unable to be shipped via Package Express. If below 50.00,
+            // will print next questions - asking user for dimensions of the package.
             System.Threading.Thread.Sleep(2000);
             Console.WriteLine("\nWhat is the weight of the package?");
             string weight = Console.ReadLine();
@@ -31,9 +30,9 @@ namespace Branching
             }
             else
             {
-                // Prints question - asking user the width and length
-                // of the package. Package's height, width, and length
-                // are then added and become the value of "dimensions".
+                // Prints questions - asking user the height, width, and length
+                // of the package. Package's height, width, and length are
+                // then added and become the value of "dimensions".
                 Console.WriteLine("\nWhat is its height?");
                 string height = Console.ReadLine();
                 double pkgHeight = Convert.ToDouble(height);
@@ -46,13 +45,11 @@ namespace Branching
                 string length = Console.ReadLine();
                 double pkgLength = Convert.ToDouble(length);
 
-                // If the dimensions are beyond 50.00, the user will be notified
-                // that it is unable to be shipped via Packge Express and 
-                // the program will end. If not, will prints notification
-                // to user that quote is being calculated.
-
                 double dimensions = pkgHeight + pkgWidth + pkgLength;
 
+                // If the dimensions are beyond 50.00, the user will be notified that
+                // it is unable to be shipped via Packge Express. If not, a message
+                // is printed, letting the user know the quote is being calculated.
                 if (dimensions > 50.00)
                 {
                     Console.WriteLine("\nPackage is too big to be shipped via Package Express. Have a good day.");
