65fa434fdc4021def2e6fa5290b62ec722d2c340
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-01 21:25:23 -0700

    Calling Methods Submission Assignment
    Calling Methods Submission Assignment

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing a message to the user; telling them to enter a number
+            // to be included in three, different math operations - 1 addition,
+            // 1 subtraction, and 1 multiplication operation.
+            Console.WriteLine("Please, select a number to include in the following math operations. \n" +
+                "It will be added, subtracted, and multiplied by 20.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            ThreeMethods method = new ThreeMethods();
+            
+            // Calling Method One and printing the math operation.
+            int totalOne = method.methodOne(userNumber);
+            Console.WriteLine("\n" + userNumber + " + " + 20 + " = " + totalOne);
+            // Calling Method Two and printing the math operation.
+            int totalTwo = method.methodTwo(userNumber);
+            Console.WriteLine("\n" + userNumber + " - " + 20 + " = " + totalTwo);
+            // Calling Method Three and printing the math operation.
+            int totalThree = method.methodThree(userNumber);
+            Console.WriteLine("\n" + userNumber + " * " + 20 + " = " + totalThree);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/ThreeMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class ThreeMethods
+    {
+        // Creating Method One; taking in the received parameter,
+        // adding it by 20, then printing the total.
+        public int methodOne(int var)
+        {
+            int totalOne = var + 20;
+            return totalOne;
+        }
+
+        // Creating Method Two; taking in the received parameter,
+        // subtracting it by 20, then printing the total.
+        public int methodTwo(int var)
+        {
+            int totalTwo = var - 20;
+            return totalTwo;
+        }
+
+        // Creating Method Three; taking in the received parameter,
+        // multiplying it by 20, then printing the total.
+        public int methodThree(int var)
+        {
+            int totalThree = var * 20;
+            return totalThree;
+        }
+    }
+}
