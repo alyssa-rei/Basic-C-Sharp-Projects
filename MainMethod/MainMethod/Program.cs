3f5f55ce5b1418866a5a15221af30ad8a64c2f1b
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-03 07:37:40 -0700

    Main Method Submission Assignment
    Main Method Submission Assignment

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing a message to the user; telling them to enter a number
+            // to be included in three, different math operations - 1 addition,
+            // 1 subtraction, and 1 multiplication operation.
+            Console.WriteLine("Please, enter an integer to be added by 20.");
+            int userInteger = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease, enter a decimal (##.##) to be subracted by 20.\n" +
+                "Answer will be rounded to the closest integer.");
+            decimal userDecimal = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\nPlease, enter an integer to be multiplied by 20.");
+            string userString = Convert.ToString(Console.ReadLine());
+
+
+            ThreeMethods method = new ThreeMethods();
+
+            // Calling the first method and printing the math operation.
+            int totalOne = method.method(userInteger);
+            Console.WriteLine("\n" + userInteger + " + " + 20 + " = " + totalOne);
+            // Calling the second method and printing the math operation.
+            int totalTwo = (int)method.method(userDecimal);
+            Console.WriteLine("\n" + userDecimal + " - " + 20 + " = " + totalTwo);
+            // Calling the third method and printing the math operation.
+            string totalThree = method.method(userString);
+            Console.WriteLine("\n" + userString + " * " + 20 + " = " + totalThree);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethod/MainMethod/ThreeMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class ThreeMethods
+    {
+        // Creating Method One; taking in the received parameter,
+        // adding it by 20, then printing the total.
+        public int method(int var)
+        {
+            int totalOne = var + 20;
+            return totalOne;
+        }
+
+        // Creating Method Two; taking in the received, decimal parameter,
+        // subtracting it by 20, then printing the total.
+        public decimal method(decimal var)
+        {
+            decimal totalTwoDecimal = var - 20;
+            int totalTwo = Convert.ToInt32(totalTwoDecimal);
+            return totalTwo;
+        }
+
+        // Creating Method Three; taking in the received parameter,
+        // multiplying it by 20, if possible, then printing the total.
+        public string method(string var)
+        {
+            try
+            {
+                int totalThreeString = Convert.ToInt32(var) * 20;
+                string totalThree = Convert.ToString(totalThreeString);
+                return totalThree;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ex.Message;
+            }
+        }
+    }
+}
