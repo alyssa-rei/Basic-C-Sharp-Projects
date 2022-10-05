4bfdb84b2078a04909b12eef0455db07843ecbef
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-05 00:32:19 -0700

    Method Class Submission Asssignment
    Method Class Submission Assignment

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Displays a message to the user, asking them to enter
+            // a number, then a second number.
+            Console.WriteLine("Please, enter your first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nEnter your second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            // "methodOne" is called from the "VoidMethod" class.
+            // The first number is passed through an equation,
+            // while the second number will display as-is,
+            // not included in any operations.
+            VoidMethod method = new VoidMethod();
+            method.methodOne(num1, num2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClass/MethodClass/VoidMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class VoidMethod
+    {
+        public void methodOne(double var1, double var2)
+        {
+            double opTotal = var1 + 30;
+            Console.WriteLine("\nYour number, {0}, plus 30 equals " + opTotal + ".", var1);
+            Console.WriteLine("\nYour second number, {0}, was not included in the equation.", var2);
+            return;
+        }
+    }
+}
