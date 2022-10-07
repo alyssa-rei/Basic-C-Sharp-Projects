316c9dd9b945b096bf4d1b900d68acb6783d0c1c
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-07 16:40:48 -0700

    Abstract Class Submission Assignment
    Abstract Class Submission Assignment

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Creating an "Employee" class and having it inherit from
+    // the "Person" class; printing "FirstName" and "LastName".
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.Write("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    // Creating an abstract class called "Person" and giving
+    // it two properties - both, string-data-types and called
+    // "FirstName" and "LastName" and declaring an abstract
+    // method called "SayName()".
+    public abstract class Person
+    {   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void SayName();
+    }
+}   
