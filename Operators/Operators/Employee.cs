f3a4f4040b1c015dfca719005426cb7514225f6c
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-09 08:15:45 -0700

    Operators Submission Assignment
    Operators Submission Assignment

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    // Creating a class called "Employee", with Id,
+    // FirstName, and LastName properties. Overloading
+    // the "==" operator to compare two Employee objects
+    // by their Id.
+    public class Employee
+    { 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return !(a.Id != b.Id);
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comparing Employee objects by their Ids
+            Employee a = new Employee() { Id = 1, FirstName = "Sample1", LastName = "Student" };
+            Employee b = new Employee() { Id = 2, FirstName = "Sample2", LastName = "Student" };
+
+            Console.WriteLine("Employees A + B have the same Id: " + (a.Id == b.Id));
+            Console.WriteLine("Employees A + B have the same First Name: " + (a.FirstName == b.FirstName));
+            Console.WriteLine("Employees A + B have the same Last Name: " + (a.LastName == b.LastName));
+
+            Console.ReadLine();
+        }
+    }
+}
