c74dbf001ff8bf751362f6840eed180bf07ae82f
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-11 21:04:54 -0700

    Lambda Submission Assignment
    Lambda Submission Assignment

---
+++ LambdaSubmission/LambdaSubmission/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaSubmission
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(int Id, string FirstName, string LastName)
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+    }
+}
