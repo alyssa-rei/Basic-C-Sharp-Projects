0082e19b4868a17ef33228d38b6cafa3edb0c534
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-15 22:21:39 -0700

    Date/Time Submission Assignment
    Date/Time Submission Assignment

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("\nPlease, enter a number:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime future = now.AddHours(hours);
+            Console.WriteLine("In {0} hours, it'll be {1}.", hours, future);
+            Console.Read();
+        }
+    }
+}
