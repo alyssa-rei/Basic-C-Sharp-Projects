c8a0c8fa2455cfa14c67f53885583f65a8d0113f
alyssa-rei <106203143+alyssa-rei@users.noreply.github.com>
2022-10-17 00:54:16 -0700

    Constructor Submission Assignment
    Constructor Submission Assignment

---
+++ ConstructorSubmission/ConstructorSubmission/Dogs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    // Creating a "Dogs" class and giving it two properties;
+    // creating constructor w/ 2 parameters and chaining two together.
+    public class Dogs
+    {
+        public string Breed { get; set; }
+        public string Weight { get; set; }
+        
+        public Dogs(string breed, string weight)
+        {
+            Breed = breed;
+            Weight = weight;
+        }
+
+        public Dogs(string breed) : this(breed, "large")
+        {
+
+        }
+    }
+}
